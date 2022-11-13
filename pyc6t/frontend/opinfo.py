28017e8e1bfd38046a618130bda1c7868b354ebe
Troy Fullwood <everyfan3417@gmail.com>
2022-11-13 17:09:14 -0500

    did most of work on enums

--- pyc6t/frontend/opinfo.py
+++ pyc6t/frontend/opinfo.py
@@ -1,52 +1,64 @@
 """C6T - C version 6 by Troy - Operator Info"""
 
-from . import lexer
+from .nlab import NLab
 
-CALL = {"call", "ucall"}
+CALL = {NLab.CALL, NLab.UCALL}
 
-ASSIGNS = {*lexer.ASSIGNS.values()}
-
-UNARY = (
-    "neg",
-    "not",
-    "addr",
-    "deref",
-    "preinc",
-    "predec",
-    "postinc",
-    "postdec",
-    "toint",
-    "toflt",
-    "compl",
-)
-
-COMMAS = {"comma", "arg"}
-
-COMMUTATIVE = {"add", "mult", "and", "or", "eor"}
-
-NEEDLVAL = {*ASSIGNS, "postinc", "preinc", "postdec", "predec", "dot"}
-
-ISLVAL = {"name", "deref", "arrow", "dot"}
-
-SUPPORTS_FLOAT = {
-    "add",
-    "sub",
-    "mult",
-    "div",
-    "less",
-    "great",
-    "lequ",
-    "gequ",
-    "equ",
-    "nequ",
-    "toint",
-    "assign",
+ASSIGNS = {
+    NLab.ASSIGN,
+    NLab.ASNADD,
+    NLab.ASNSUB,
+    NLab.ASNMULT,
+    NLab.ASNDIV,
+    NLab.ASNMOD,
+    NLab.ASNRSHIFT,
+    NLab.ASNLSHIFT,
+    NLab.ASNAND,
+    NLab.ASNEOR,
+    NLab.ASNOR,
 }
 
-LESSGREAT = {"less", "great", "lequ", "gequ"}
+UNARY = (
+    NLab.NEG,
+    NLab.NOT,
+    NLab.ADDR,
+    NLab.DEREF,
+    NLab.PREINC,
+    NLab.PREDEC,
+    NLab.POSTINC,
+    NLab.POSTDEC,
+    NLab.TOINT,
+    NLab.TOFLT,
+    NLab.COMPL,
+)
 
-CMP = {*LESSGREAT, "equ", "nequ"}
+COMMAS = {NLab.COMMA, NLab.ARG}
 
-ISINT = {*CMP}
+COMMUTATIVE = {NLab.ADD, NLab.MULT, NLab.AND, NLab.OR, NLab.EOR}
 
-NOFLTOP = {"toint", "toflt", "dot", "arrow"}
+NEEDLVAL = {*ASSIGNS, NLab.POSTINC, NLab.PREINC, NLab.POSTDEC, NLab.PREDEC, NLab.DOT}
+
+ISLVAL = {NLab.NAME, NLab.DEREF, NLab.ARROW, NLab.DOT}
+
+SUPPORTS_FLOAT = {
+    NLab.ADD,
+    NLab.SUB,
+    NLab.MULT,
+    NLab.DIV,
+    NLab.LESS,
+    NLab.GREAT,
+    NLab.LEQU,
+    NLab.GEQU,
+    NLab.EQU,
+    NLab.NEQU,
+    NLab.TOINT,
+    NLab.ASSIGN,
+}
+
+LESSGREAT = {NLab.LESS, NLab.GREAT, NLab.LEQU, NLab.GEQU}
+
+CMP = {*LESSGREAT, NLab.EQU, NLab.NEQU}
+
+ISINT = {*CMP, NLab.LOGOR, NLab.LOGAND}
+
+NOFLTOP = {NLab.TOINT, NLab.TOFLT, NLab.DOT, NLab.ARROW}
